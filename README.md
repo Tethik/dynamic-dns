3b5c73f511f4d3daaccaec965efe39392ac8109d
Joakim Uddholm <joakim@uddholm.com>
2016-04-07 05:21:26 +0200

    webservice updated.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dynamic-dns
 Dynamic-DNS REST API using the GleSyS API.
+
+
+```
+curl --data "token=09fd19a4-0b71-47b5-a6fd-8ca75b4fcaaa&hostname=test.d.blacknode.se" http://localhost:8000/update
+```
--- dynamic-dns/wsgi.py
+++ dynamic-dns/wsgi.py
@@ -1,15 +1,18 @@
 import falcon
 import json
+from service import DNSService
+from config import Config
+import db
 
 class DNSResource:
-    def __init__(self, db):
-        self.db = db
-
-    def on_get(self, req, resp):
-        pass
+    def __init__(self, service):
+        self.service = service
 
     def on_post(self, req, resp):
-        pass
+        token = req.get_param("token", required=True)
+        hostname = req.get_param("hostname", required=True)
+        ip = req.get_param("ip") or req.env['REMOTE_ADDR']
+        self.service.update_dns_record(token, hostname, ip)
 
 class IPResource:
     def on_get(self, req, resp):
@@ -17,6 +20,8 @@ class IPResource:
         # resp.body = json.dumps(req.env)
 
 
-app = falcon.API()
-app.add_route('/ip', IPResource())
-# app.add_route('/update', DNSResource())
+def webservice(service):
+    app = falcon.API()
+    app.add_route('/ip', IPResource())
+    app.add_route('/update', DNSResource(service))
+    return app
